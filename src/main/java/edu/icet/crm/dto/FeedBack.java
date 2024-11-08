842dd3aa6b7c412935bfa1763842e552a6e640b9
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-08 14:43:22 +0530

    feedback table added

---
+++ src/main/java/edu/icet/crm/dto/FeedBack.java
@@ -0,0 +1,15 @@
+package edu.icet.crm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedBack {
+    private Integer id;
+    private String description;
+}
---
+++ src/main/java/edu/icet/crm/entity/FeedBackEntity.java
@@ -0,0 +1,22 @@
+package edu.icet.crm.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "FeedBack")
+public class FeedBackEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String description;
+}
