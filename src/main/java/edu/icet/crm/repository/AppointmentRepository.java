0fa10992ebaf33ebf9139b5ab81fdeffc08113fd
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-21 22:39:10 +0530

    change repository classes

--- src/main/java/edu/icet/crm/repository/AppointmentRepository.java
+++ src/main/java/edu/icet/crm/repository/AppointmentRepository.java
@@ -1,8 +1,7 @@
 package edu.icet.crm.repository;
 
 import edu.icet.crm.entity.AppointmentEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface AppointmentRepository extends CrudRepository<AppointmentEntity,Long> {
+public interface AppointmentRepository extends JpaRepository<AppointmentEntity,Long> {
 }
--- src/main/java/edu/icet/crm/repository/CustomerAccRepository.java
+++ src/main/java/edu/icet/crm/repository/CustomerAccRepository.java
@@ -4,6 +4,9 @@ import edu.icet.crm.entity.CustomerAccEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerAccRepository extends CrudRepository<CustomerAccEntity,Integer> {
+    Optional<CustomerAccEntity> findByEmail(String email);
 }
